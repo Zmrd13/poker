36c66ea9800e3bb21cba8e8cc5ba4d7f20f5cdf6
Egor <weders@pop-os.localdomain>
2022-11-27 23:08:37 +0700

    Refactor (added constants and formatted)

--- crypt.h
+++ crypt.h
@@ -5,9 +5,10 @@
 
 #ifndef INF_CRYPT_H
 #define INF_CRYPT_H
-
-#include <vector>
+#pragma once
+#include <iostream>
 #include <cmath>
+#include <vector>
 /**
  * Библиотека по методичке защиты
  *
@@ -17,20 +18,14 @@
 using lli = long long int;
 using namespace std;
 
-template<typename T>
-void nlPrint(T data) {
-
-    cout << endl << data << endl;
-
-
-};
+template <typename T> void nlPrint(T data) { cout << endl << data << endl; };
 
 void nlPrint(const vector<lli> &data) {
-    cout << endl;
-    for (auto &i: (vector<lli>) data) {
-        cout << i << " ";
-    }
-    cout << endl;
+  cout << endl;
+  for (auto &i : (vector<lli>)data) {
+    cout << i << " ";
+  }
+  cout << endl;
 }
 
 /**
@@ -43,28 +38,29 @@ void nlPrint(const vector<lli> &data) {
  *
  */
 lli modPow(lli iBase, lli iPow, lli iMod) {
-    if (iMod == 0) {
-        return 0;
+  if (iMod == 0) {
+    return 0;
+  }
+  vector<lli> vecMod = {};
+  lli logPow = (lli)log2((double)iPow);
+  vecMod.push_back(iBase % iMod);
+  for (lli i = 1; i <= logPow; i++) {
+    vecMod.push_back(((vecMod.at(i - 1) % iMod) * (vecMod.at(i - 1) % iMod)) %
+                     iMod);
+  }
+  vector<lli> vecBinPow = {};
+  while (iPow >= 1) {
+    vecBinPow.push_back(iPow % 2);
+    iPow /= 2;
+  }
+  lli iResult = 1;
+  for (lli i = 0; i < vecBinPow.size(); i++) {
+    if (vecBinPow.at(i) == 1) {
+      iResult *= (vecMod.at(i) % iMod) % iMod;
+      iResult %= iMod;
     }
-    vector<lli> vecMod = {};
-    lli logPow = (lli) log2((double) iPow);
-    vecMod.push_back(iBase % iMod);
-    for (lli i = 1; i <= logPow; i++) {
-        vecMod.push_back(((vecMod.at(i - 1) % iMod) * (vecMod.at(i - 1) % iMod)) % iMod);
-    }
-    vector<lli> vecBinPow = {};
-    while (iPow >= 1) {
-        vecBinPow.push_back(iPow % 2);
-        iPow /= 2;
-    }
-    lli iResult = 1;
-    for (lli i = 0; i < vecBinPow.size(); i++) {
-        if (vecBinPow.at(i) == 1) {
-            iResult *= (vecMod.at(i) % iMod) % iMod;
-            iResult %= iMod;
-        }
-    }
-    return iResult % iMod;
+  }
+  return iResult % iMod;
 }
 
 /**
@@ -75,21 +71,21 @@ lli modPow(lli iBase, lli iPow, lli iMod) {
  *
  */
 vector<lli> extGCD(lli iA, lli iB) {
-    lli q = 0;
-    vector<lli> vecU = {iA, 1, 0};
-    vector<lli> vecV = {iB, 0, 1};
-    vector<lli> vecT = {};
+  lli q = 0;
+  vector<lli> vecU = {iA, 1, 0};
+  vector<lli> vecV = {iB, 0, 1};
+  vector<lli> vecT = {};
 
-    if (iA < iB) {
-        return {};
-    }
-    while (vecV[0] != 0) {
-        q = vecU[0] / vecV[0];
-        vecT = {vecU[0] % vecV[0], vecU[1] - q * vecV[1], vecU[2] - q * vecV[2]};
-        vecU = vecV;
-        vecV = vecT;
-    }
-    return vecU;
+  if (iA < iB) {
+    return {};
+  }
+  while (vecV[0] != 0) {
+    q = vecU[0] / vecV[0];
+    vecT = {vecU[0] % vecV[0], vecU[1] - q * vecV[1], vecU[2] - q * vecV[2]};
+    vecU = vecV;
+    vecV = vecT;
+  }
+  return vecU;
 }
 
 /**
@@ -103,12 +99,12 @@ vector<lli> extGCD(lli iA, lli iB) {
  * */
 bool checkSimple(lli iN) {
 
-    for (int i = 2; i <= sqrt(iN); i++) {
-        if (iN % i == 0) {
-            return false;
-        }
+  for (int i = 2; i <= sqrt(iN); i++) {
+    if (iN % i == 0) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
 /**
@@ -120,60 +116,55 @@ bool checkSimple(lli iN) {
  * @return Common random gen key for Bob and Alice
  *
  * */
-lli diffHelm(int iSeed = NULL) {
-    if (iSeed != 0) {
-        srand(iSeed);
-    } else {
-        srand(time(NULL));
-    }
-    lli iQ = 0;
-    lli iP = 0;
-    lli iG = 0;
-    do {
-        iQ = rand() % ((lli) pow(10, 9) - 1) + 1;
-        iP = 2 * iQ + 1;
-    } while (!(checkSimple(iQ) && checkSimple(iP)));
+lli diffHelm(int iSeed = 0) {
+  if (iSeed != 0) {
+    srand(iSeed);
+  } else {
+    srand(time(NULL));
+  }
+  lli iQ = 0;
+  lli iP = 0;
+  lli iG = 0;
+  do {
+    iQ = rand() % ((lli)pow(10, 9) - 1) + 1;
+    iP = 2 * iQ + 1;
+  } while (!(checkSimple(iQ) && checkSimple(iP)));
 
+  do {
+    iG = rand() % (iP - 2) + 2;
+  } while (modPow(iG, iQ, iP) == 1);
 
-    do {
-        iG = rand() % (iP - 2) + 2;
-    } while (modPow(iG, iQ, iP) == 1);
+  nlPrint("G argument");
+  nlPrint(iG);
+  nlPrint("P argument");
+  nlPrint(iP);
+  lli iAliceSecKey = rand() % (iP - 2) + 2;
+  lli iBobSecKey = rand() % (iP - 2) + 2;
+  nlPrint("Alice secret:");
+  nlPrint(iAliceSecKey);
+  nlPrint("Bob secret:");
+  nlPrint(iBobSecKey);
+  lli iAlicePubKey = modPow(iG, iAliceSecKey, iP);
+  lli iBobPubKey = modPow(iG, iBobSecKey, iP);
 
+  lli iAliceCommKey = modPow(iBobPubKey, iAliceSecKey, iP);
+  lli iBobCommKey = modPow(iAlicePubKey, iBobSecKey, iP);
 
-    nlPrint("G argument");
-    nlPrint(iG);
-    nlPrint("P argument");
-    nlPrint(iP);
-    lli iAliceSecKey = rand() % (iP - 2) + 2;
-    lli iBobSecKey = rand() % (iP - 2) + 2;
-    nlPrint("Alice secret:");
-    nlPrint(iAliceSecKey);
-    nlPrint("Bob secret:");
-    nlPrint(iBobSecKey);
-    lli iAlicePubKey = modPow(iG, iAliceSecKey, iP);
-    lli iBobPubKey = modPow(iG, iBobSecKey, iP);
-
-    lli iAliceCommKey = modPow(iBobPubKey, iAliceSecKey, iP);
-    lli iBobCommKey = modPow(iAlicePubKey, iBobSecKey, iP);
-
-    if (iAliceCommKey == iBobCommKey) {
-        nlPrint("Common:");
-        nlPrint(iAliceCommKey);
-        return iAliceCommKey;
-    } else {
-        cout << "ERROR:" << endl;
-        nlPrint("Alice common:");
-        nlPrint(iAliceCommKey);
-        nlPrint("Bob common:");
-        nlPrint(iBobCommKey);
-        return -1;
-    }
-
+  if (iAliceCommKey == iBobCommKey) {
+    nlPrint("Common:");
+    nlPrint(iAliceCommKey);
+    return iAliceCommKey;
+  } else {
+    cout << "ERROR:" << endl;
+    nlPrint("Alice common:");
+    nlPrint(iAliceCommKey);
+    nlPrint("Bob common:");
+    nlPrint(iBobCommKey);
+    return -1;
+  }
+  return -1;
 }
-lli random(lli min, lli max) {
-
-    return rand() % (max - min) + min;
-}
+lli random(lli min, lli max) { return rand() % (max - min) + min; }
 /**
  * Baby-step giant-step y=a^x mod p
  * @param iA- a/b in formula
@@ -182,62 +173,60 @@ lli random(lli min, lli max) {
  *
  * */
 lli BCStep(lli iA, lli iP, lli iY) {
-    lli iM = sqrt(iP) + 1, iK = iM;
-    vector<lli> vecCalc = {};
-    vector<lli> vecAns;
-    lli iNumber = 0;
-    for (lli i = 0; i <= iM - 1; i++) {
-        vecCalc.push_back((modPow(iA, i, iP) * (iY % iP)) % iP);
+  lli iM = sqrt(iP) + 1, iK = iM;
+  vector<lli> vecCalc = {};
+  vector<lli> vecAns;
+  lli iNumber = 0;
+  for (lli i = 0; i <= iM - 1; i++) {
+    vecCalc.push_back((modPow(iA, i, iP) * (iY % iP)) % iP);
+  }
+  for (lli i = 1; i <= iK; i++) {
+    iNumber = modPow(iA, i * iM, iP);
+    for (lli j = 0; j < vecCalc.size(); j++) {
+      if (iNumber == vecCalc[j]) {
+
+        vecAns.push_back(i * iM - j);
+      }
     }
-    for (lli i = 1; i <= iK; i++) {
-        iNumber = modPow(iA, i * iM, iP);
-        for (lli j = 0; j < vecCalc.size(); j++) {
-            if (iNumber == vecCalc[j]) {
+  }
 
-                vecAns.push_back(i * iM - j);
-            }
-        }
-    }
+  //    for (int i = 0; i < vecAns.size(); i++) {
+  //        if (modPow(iA, vecAns[i], iP) == iY) {
+  //            cout << "(+)";
+  //        }
+  //        cout << vecAns[i] << " ";
+  //    }
+  nlPrint("Intersections");
+  nlPrint(vecAns);
+  cout << endl;
 
-
-//    for (int i = 0; i < vecAns.size(); i++) {
-//        if (modPow(iA, vecAns[i], iP) == iY) {
-//            cout << "(+)";
-//        }
-//        cout << vecAns[i] << " ";
-//    }
-    nlPrint("Intersections");
-    nlPrint(vecAns);
-    cout << endl;
-
-    return 0;
+  return 0;
 }
 lli genPrime(lli min = 4, lli max = 100) {
-    lli temp = 4;
-    while (!checkSimple(temp)) {
-        temp = random(min, max);
-
-    }
-    return temp;
+  lli temp = 4;
+  while (!checkSimple(temp)) {
+    temp = random(min, max);
+  }
+  return temp;
 }
 lli invEv(lli a, lli b) {
-    if ((a % b * (extGCD(b, a).at(2) + b)) % b) {
+  if ((a % b * (extGCD(b, a).at(2) + b)) % b) {
 
-        return (extGCD(b, a).at(2) + b);
-    }
-    return 0;
+    return (extGCD(b, a).at(2) + b);
+  }
+  return 0;
 }
 lli genMutPrime(lli p) {
-    lli check = 0, d;
+  lli check = 0, d;
 
-    while (check == 0) {
-        d = random(4, p);
+  while (check == 0) {
+    d = random(4, p);
 
-        if (extGCD(p, d).at(0) == 1) {
-            check = 1;
-        }
+    if (extGCD(p, d).at(0) == 1) {
+      check = 1;
     }
-    return d;
+  }
+  return d;
 }
 
-#endif //INF_CRYPT_H
+#endif // INF_CRYPT_H
--- main.cpp
+++ main.cpp
@@ -1,236 +1,210 @@
-#include <iostream>
-#include <vector>
+#include "crypt.h"
 #include <algorithm>
 #include <random>
 #include <set>
-#include "crypt.h"
+#include <vector>
 
 const int MAX_PLAYERS = 11;
+const int MAX_CARDS = 52;
+
 using namespace std;
 using lli = long long int;
 struct card {
-    int number = -1;
-    int suit = -1;
-    int name = -1;
-    string suitStr;
-    string nameStr;
+  int number = -1;
+  int suit = -1;
+  int name = -1;
+  string suitStr;
+  string nameStr;
 };
 struct player {
-    int number;
-    lli C, D;//Keys
-    lli hand[2];
+  int number;
+  lli C, D; // Keys
+  lli hand[2];
 };
 
 player playerInit(lli P) {
-    player temp{};
-    temp.C = genMutPrime(P - 1);
-    temp.D = invEv(temp.C, P - 1);
-
-    return temp;
+  player temp{};
+  temp.C = genMutPrime(P - 1);
+  temp.D = invEv(temp.C, P - 1);
+  return temp;
 }
 
 card readCard(lli input) {
-    card temp;
-    if (input <= 0) {
-        return {};
-    }
-    input -= 2;
-    temp.number = input;
-    temp.suit = input % 4;
-    switch (temp.suit) {
-        case 0:
-            temp.suitStr = "Diamonds";
-            break;
-        case 1:
-            temp.suitStr = "Hearts";
-            break;
-        case 2:
-            temp.suitStr = "Clubs";
-            break;
-        case 3:
-            temp.suitStr = "Spades";
-            break;
-    }
-    temp.name = input % 13;
-    switch (temp.name) {
-        case 0:
-            temp.nameStr = "Ace";
-            break;
-        case 1:
-            temp.nameStr = "2";
-            break;
-        case 2:
-            temp.nameStr = "3";
-            break;
-        case 3:
-            temp.nameStr = "4";
-            break;
-        case 4:
-            temp.nameStr = "5";
-            break;
-        case 5:
-            temp.nameStr = "6";
-            break;
-        case 6:
-            temp.nameStr = "7";
-            break;
-        case 7:
-            temp.nameStr = "8";
-            break;
-        case 8:
-            temp.nameStr = "9";
-            break;
-        case 9:
-            temp.nameStr = "10";
-            break;
-        case 10:
-            temp.nameStr = "Jack";
-            break;
-        case 11:
-            temp.nameStr = "Queen";
-            break;
-        case 12:
-            temp.nameStr = "King";
-            break;
-
-    }
-    return temp;
+  card temp;
+  if (input <= 0) {
+    return {};
+  }
+  input -= 2;
+  temp.number = input;
+  temp.suit = input % 4;
+  switch (temp.suit) {
+  case 0:
+    temp.suitStr = "Diamonds";
+    break;
+  case 1:
+    temp.suitStr = "Hearts";
+    break;
+  case 2:
+    temp.suitStr = "Clubs";
+    break;
+  case 3:
+    temp.suitStr = "Spades";
+    break;
+  }
+  temp.name = input % 13;
+  switch (temp.name) {
+  case 0:
+    temp.nameStr = "Ace";
+    break;
+  case 1:
+    temp.nameStr = "2";
+    break;
+  case 2:
+    temp.nameStr = "3";
+    break;
+  case 3:
+    temp.nameStr = "4";
+    break;
+  case 4:
+    temp.nameStr = "5";
+    break;
+  case 5:
+    temp.nameStr = "6";
+    break;
+  case 6:
+    temp.nameStr = "7";
+    break;
+  case 7:
+    temp.nameStr = "8";
+    break;
+  case 8:
+    temp.nameStr = "9";
+    break;
+  case 9:
+    temp.nameStr = "10";
+    break;
+  case 10:
+    temp.nameStr = "Jack";
+    break;
+  case 11:
+    temp.nameStr = "Queen";
+    break;
+  case 12:
+    temp.nameStr = "King";
+    break;
+  }
+  return temp;
 }
 
-vector<lli> shuffleCards(player pl, vector<lli> cards, lli P) {    auto rng = default_random_engine {};
-    shuffle(begin(cards), std::end(cards), rng);
-    vector<lli> shCards(cards.size());
-    for (int i = 0; i < cards.size(); i++) {
-        shCards.at(i) = modPow(cards.at(i), pl.C, P);
-
-    }
-    return shCards;
+vector<lli> shuffleCards(player pl, vector<lli> cards, lli P) {
+  auto rng = default_random_engine{};
+  shuffle(begin(cards), std::end(cards), rng);
+  vector<lli> shCards(cards.size());
+  for (int i = 0; i < cards.size(); i++) {
+    shCards.at(i) = modPow(cards.at(i), pl.C, P);
+  }
+  return shCards;
 }
 
 vector<lli> deshuffleCards(player pl, vector<lli> cards, lli P) {
 
-    vector<lli> shCards(cards.size());
-    for (int i = 0; i < cards.size(); i++) {
-        shCards.at(i) = modPow(cards.at(i), pl.D, P);
-
-    }
-    return shCards;
+  vector<lli> shCards(cards.size());
+  for (int i = 0; i < cards.size(); i++) {
+    shCards.at(i) = modPow(cards.at(i), pl.D, P);
+  }
+  return shCards;
 }
 
 int playerCount() {
-    int t = 90;
-
-    while (t >= MAX_PLAYERS || t <= 0) {
-        cout << "Enter number of players(0<count<11)\n:";
-        cin >> t;
-    }
-    return t;
+  int t = MAX_PLAYERS;
+  while (t >= MAX_PLAYERS || t <= 0) {
+    cout << "Enter number of players(0<count<11)\n:";
+    cin >> t;
+  }
+  return t;
 }
 
 card readEncCard(vector<player> deck, player pl, lli encCard, lli P) {
-    lli temp = encCard;
-    for (auto &i: deck) {
-        if (i.number != pl.number) {
-            temp = modPow(temp, i.D, P);
-
-        }
+  lli temp = encCard;
+  for (auto &i : deck) {
+    if (i.number != pl.number) {
+      temp = modPow(temp, i.D, P);
     }
-    temp = modPow(temp, pl.D, P);
-    return readCard(temp);
+  }
+  temp = modPow(temp, pl.D, P);
+  return readCard(temp);
 }
 
 int main() {
-
-    vector<lli> duplCheck={};
-    srand(time(NULL));
-
-    vector<lli> cards(52);
-    for (int i = 0; i < 52; i++) {
-        cards.at(i) = i + 2;
-        //1   cout<<readCard(i).suitStr<<" "<<readCard(i).nameStr<<"\n";
-    }
-    lli P = genPrime(INT16_MAX, INT32_MAX);
-    nlPrint(P);
-    int count = playerCount();
-    cout << count;
-    vector<player> deck(count);
-    for (int i = 0; i < count; i++) {
-        deck.at(i) = (playerInit(P));
-        deck.at(i).number = i;
-    }
-    for (auto &i: deck) {
-        cards = shuffleCards(i, cards, P);
-    }
-    nlPrint("");
-    for (auto &i: cards) {
-        cout << i << " ";
-
-    }
-    for (auto &i: deck) {
-        for (int j = 0; j < 2; j++) {
-            bool grabbed = false;
-            while (!grabbed) {
-                int grab = rand() % cards.size();
-                if (cards.at(grab) > 0) {
-                    duplCheck.push_back(cards.at(grab));
-                    i.hand[j] = cards.at(grab);
-                    cards.at(grab) *= -1;
-                    grabbed = true;
-                }
-            }
-
+  vector<lli> cards(MAX_CARDS);
+  vector<lli> onDeckCards = {};
+  vector<lli> duplCheck = {};
+  srand(time(NULL));
+  lli P = genPrime(INT16_MAX, INT32_MAX);
+  int count = playerCount();
+  vector<player> deck(count);
+  for (int i = 0; i < MAX_CARDS; i++) {
+    cards.at(i) = i + 2;
+  }
+  for (int i = 0; i < count; i++) {
+    deck.at(i) = (playerInit(P));
+    deck.at(i).number = i;
+  }
+  for (auto &i : deck) {
+    cards = shuffleCards(i, cards, P);
+  }
+  for (auto &i : deck) {
+    for (int j = 0; j < 2; j++) {
+      bool grabbed = false;
+      while (!grabbed) {
+        int grab = rand() % cards.size();
+        if (cards.at(grab) > 0) {
+          duplCheck.push_back(cards.at(grab));
+          i.hand[j] = cards.at(grab);
+          cards.at(grab) *= -1;
+          grabbed = true;
         }
-
-
+      }
     }
-    nlPrint("Cards" + to_string(cards.size()));
-    nlPrint("Players");
-    for (auto &i: deck) {
-        nlPrint(readEncCard(deck, i, i.hand[0], P).nameStr + " " + readEncCard(deck, i, i.hand[0], P).suitStr+"    "+readEncCard(deck, i, i.hand[1], P).nameStr + " " + readEncCard(deck, i, i.hand[1], P).suitStr);
-        cout<<"______________________________________________________________________________________";
-        //nlPrint();
-        // nlPrint(readEncCard(deck,i,i.hand[1],P).number);
-        //   nlPrint(readEncCard(deck,i,i.hand[0],P).number);
-
+  }
+  nlPrint("Cards" + to_string(cards.size()));
+  nlPrint("Players");
+  for (auto &i : deck) {
+    nlPrint(readEncCard(deck, i, i.hand[0], P).nameStr + " " +
+            readEncCard(deck, i, i.hand[0], P).suitStr + "   " +
+            readEncCard(deck, i, i.hand[1], P).nameStr + " " +
+            readEncCard(deck, i, i.hand[1], P).suitStr);
+    cout
+        << "__________________________________________________________________";
+  }
+  nlPrint("On Deck Cards________________________________________");
+  for (int j = 0; j < 5; j++) {
+    bool grabbed = false;
+    while (!grabbed) {
+      int grab = rand() % cards.size();
+      if (cards.at(grab) > 0) {
+        duplCheck.push_back(cards.at(grab));
+        onDeckCards.push_back(cards.at(grab));
+        cards.at(grab) *= -1;
+        grabbed = true;
+      }
     }
-    nlPrint("On Deck Cards________________________________________");
-    vector<lli> onDeckCards={};
+  }
 
-        for (int j = 0; j < 5; j++) {
-            bool grabbed = false;
-            while (!grabbed) {
-                int grab = rand() % cards.size();
-                if (cards.at(grab) > 0) {
-                    duplCheck.push_back(cards.at(grab));
-                     onDeckCards.push_back(cards.at(grab));
-                    cards.at(grab) *= -1;
-                    grabbed = true;
-                }
-            }
+  onDeckCards = deshuffleCards(deck.at(0), onDeckCards, P);
+  for (auto &i : onDeckCards) {
+    cout << "|" << readCard(i).nameStr + " " + readCard(i).suitStr << "|  ";
+  }
 
-
-
-    }
-
-        onDeckCards= deshuffleCards(deck.at(0),onDeckCards,P);
-    for (auto &i: onDeckCards) {
-            cout<<"|"<<readCard(i).nameStr + " " + readCard(i).suitStr<<"|  ";
-    }
-    ////ПРОВЕРКА НА КОРРЕКТНОСТЬ ВЫБОРКИ И УДАЛЕНИЯ
-    duplCheck.insert(duplCheck.end(),cards.begin(),cards.end());
-    duplCheck= deshuffleCards(deck.at(0),duplCheck,P);
-//    nlPrint("Remaining cards"); q
-    duplCheck.erase(std::remove_if(
-            duplCheck.begin(),duplCheck.end(),
-            [](const lli& x) {
-                return x < 0; // put your condition here
-            }), duplCheck.end());
-//    for (auto &i: duplCheck) {
-//        if (i > 0)
-//            nlPrint(readCard(i).nameStr + " " + readCard(i).suitStr);
-//    }
-    set<int> s(duplCheck.begin(), duplCheck.end());
-    nlPrint("Cards in use "+ to_string(duplCheck.size()));
-    nlPrint("Unique cards on deck  "+ to_string(duplCheck.size()));
-    return 0;
+  ////ПРОВЕРКА НА КОРРЕКТНОСТЬ ВЫБОРКИ И УДАЛЕНИЯ
+  duplCheck.insert(duplCheck.end(), cards.begin(), cards.end());
+  duplCheck = deshuffleCards(deck.at(0), duplCheck, P);
+  //    nlPrint("Remaining cards"); q
+  duplCheck.erase(std::remove_if(duplCheck.begin(), duplCheck.end(),
+                                 [](const lli &x) { return x < 0; }),
+                  duplCheck.end());
+  set<int> s(duplCheck.begin(), duplCheck.end());
+  nlPrint("Cards in use " + to_string(duplCheck.size()));
+  nlPrint("Unique cards on deck  " + to_string(duplCheck.size()));
+  return 0;
 }
